43e33e47040b2558663dce3a7c28c98675ca6d41
delectablerec <info@delectablerecords.com>
2024-10-11 09:40:11 +0200

    caricata la prima esercitazione e la solution

---
+++ esercitazione_01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Console.WriteLine("Hello World!");
+Console.WriteLine("Inserisci il tuo nome: "); // stampo un messaggio a video
+// Console.ReadKey();
+string? nome = Console.ReadLine(); // definisco una variabile di tipo string e la inizializzo con il valore inserito dall'utente\
+Console.WriteLine($"Ciao {nome}"); // stampo il valore della variabile nome
+                                    // usando l'interpolazione delle stringhe
+Console.WriteLine("Schiaccia un tasto per terminare il programma");
+Console.ReadKey(); // attendo la pressione di un tasto da parte dell'utente
