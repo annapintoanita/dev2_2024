c591ca561b14caf19a5e7112a56c5df9d8cbadfc
delectablerec <info@delectablerecords.com>
2024-10-11 09:53:48 +0200

    aggiunta documentazione

--- esercitazione_01/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Inserisci il tuo nome: "); // stampo un messaggio a video
-string? nome = Console.ReadLine(); // definisco una variabile di tipo string e la inizializzo con il valore inserito dall'utente\
-Console.WriteLine($"Ciao {nome}"); // stampo il valore della variabile nome
-                                    // usando l'interpolazione delle stringhe
-Console.WriteLine("Schiaccia un tasto per terminare il programma");
-Console.ReadKey(); // attendo la pressione di un tasto da parte dell'utente
