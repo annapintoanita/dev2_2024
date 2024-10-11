48b6a98e0940dd9811d3dd91b409677283c52b86
delectablerec <info@delectablerecords.com>
2024-10-11 09:45:14 +0200

    modificato program cs togliendo i commenti

--- esercitazione_01/Program.cs
+++ esercitazione_01/Program.cs
@@ -1,6 +1,4 @@
-﻿// Console.WriteLine("Hello World!");
-Console.WriteLine("Inserisci il tuo nome: "); // stampo un messaggio a video
-// Console.ReadKey();
+﻿Console.WriteLine("Inserisci il tuo nome: "); // stampo un messaggio a video
 string? nome = Console.ReadLine(); // definisco una variabile di tipo string e la inizializzo con il valore inserito dall'utente\
 Console.WriteLine($"Ciao {nome}"); // stampo il valore della variabile nome
                                     // usando l'interpolazione delle stringhe
