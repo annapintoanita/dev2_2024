ff2f2bf7fdc6918f9cdcef89b8703ef857cffbf3
delectablerec <info@delectablerecords.com>
2024-10-22 16:57:25 +0200

    Indovina Numero: Versione 3

--- Assignment/01_indovina-numero/Program.cs
+++ Assignment/01_indovina-numero/Program.cs
@@ -1,29 +1,40 @@
 ﻿Random random = new Random(); // Random e la classe che genera numeri casuali
 int numeroDaIndovinare = random.Next(1, 101); // Next e il metodo che genera un numero casuale tra 1 e 100
+int numeroInserito;
+int tentativiMassimi = 5;  
+int tentativiEffettuati = 0;  
+bool haIndovinato = false;
+int numeroUtente = 0;
 
 Console.Clear();
 
-Console.WriteLine("Indovina il numero (tra 1 e 100): ");
+Console.WriteLine("Indovina il numero (tra 1 e 100). Hai 5 tentativi.");
 
-int numeroInserito;
-
-numeroInserito = 0; // Inizializzo a 0 per entrare nel ciclo while
-
-while (numeroInserito != numeroDaIndovinare)
-{
-    numeroInserito = Convert.ToInt32(Console.ReadLine());
-
-    if (numeroInserito < numeroDaIndovinare)
-    {
-        Console.WriteLine("Il numero da indovinare e' maggiore");
-    }
-    else
-    {
-        Console.WriteLine("Il numero da indovinare e' minore");
-    }
-
-    Console.WriteLine("Riprova: ");
+while (tentativiEffettuati < tentativiMassimi && !haIndovinato)  
+{  
+    Console.Write("Tentativo {0}: ", tentativiEffettuati + 1);  
     
-}
+    // numeroUtente = int.Parse(Console.ReadLine());  
+    numeroUtente = int.Parse(Console.ReadLine());
+    
+    tentativiEffettuati++;
 
-Console.WriteLine($"Hai indovinato! Il numero da indovinare era {numeroDaIndovinare}");
+    if (numeroUtente < numeroDaIndovinare)  
+    {  
+        Console.WriteLine("Il numero da indovinare è maggiore.");  
+    }  
+    else if (numeroUtente > numeroDaIndovinare)  
+    {  
+        Console.WriteLine("Il numero da indovinare è minore.");  
+    }  
+    else  
+    {  
+        Console.WriteLine("Complimenti! Hai indovinato il numero.");  
+        haIndovinato = true;  
+    }
+
+    if (!haIndovinato && tentativiEffettuati == tentativiMassimi)  
+    {  
+        Console.WriteLine("Hai esaurito i tentativi. Il numero era " + numeroDaIndovinare + ".");  
+    }  
+}
