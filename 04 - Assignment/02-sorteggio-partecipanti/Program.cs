f224bedaf98de52baf3c0a74fc0e321eeb923b36
annapintoanita <annapintoanita03@gmail.com>
2024-11-05 10:56:45 +0100

    sorteggio versione 1

---
+++ 04 - Assignment/02-sorteggio-partecipanti/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//creo la lista di nomi dei partecipaanti
+List<string>listaPartecipanti= new List<string>
+{
+    "Andrea", "Anita", "Diego", "Felipe", "Giorgio", "Ivan", "Sofia", "Tamer"
+    };
+    foreach (var nome in listaPartecipanti) // utilizzo il foreach per stamparli tutti
+    {
+        Console.WriteLine(nome);
+    }
+ Random random = new Random();//metodo random per sorteggiare i nomi casualmente 
+
+ int nomeCasuale= random.Next(listaPartecipanti.Count); // mi sono creata un indice
+
+  Console.WriteLine("E' stato sorteggiato " + listaPartecipanti[nomeCasuale] ); // ho inserito l'indice nel Console.WriteLine
+  
