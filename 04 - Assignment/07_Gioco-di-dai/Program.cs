56a7c6f0d748832d32ce06bdbb2f79a5dc746c11
annapintoanita <annapintoanita03@gmail.com>
2024-11-14 16:57:05 +0100

    implemento qualche funzione

--- 04 - Assignment/07_Gioco-di-dai/Program.cs
+++ 04 - Assignment/07_Gioco-di-dai/Program.cs
@@ -1,37 +1,55 @@
-﻿string rispostaUtente;
+﻿
+string rispostaUtente;
 do
 {
     //genero numeri random 
     Random random = new Random();
-// presento il gioco all'utente
-Console.WriteLine("Benvenuto nel gioco dei dadi. Chi fa il punteggio più alto vince il gioco");
+    // presento il gioco all'utente
+    Console.WriteLine("Benvenuto nel gioco dei dadi. Chi fa il punteggio più alto vince il gioco");
 
-Console.WriteLine("Scrivi 'lancio' per lanciare il dado");
-Console.ReadLine();
-//l'utente tira
-int tiroUtente= random.Next(1,7);
-Console.WriteLine($"è uscito il numero {tiroUtente}");
-// il computer tira
-Console.WriteLine("Tocca a me..Sto lanciando il dado ");
-int tiroComputer = random.Next(1,7);
-Console.WriteLine($"mi è uscito il numero {tiroComputer}");
-if (tiroUtente > tiroComputer)
+    Console.WriteLine("Scrivi 'lancio' per lanciare il dado");
+    Console.ReadLine();
+    //l'utente tira
+    int tiroUtente = LancioDado(random);
+
+    // il computer tira
+    Console.WriteLine("Tocca a me..Sto lanciando il dado ");
+    int tiroComputer = LancioDado(random);
+    StampaVincitore(tiroUtente, tiroComputer);
+
+    Console.WriteLine("vuoi riprovare? s/n");
+    rispostaUtente = Console.ReadLine();
+}
+while (rispostaUtente == "s");
+
+#region Funzioni
+
+// La funzione deve richiamare sia il tiro utente che il tiro computer
+int LancioDado(Random random)
 {
-    Console.WriteLine("HAI VINTO TU");
+    int dado = random.Next(1, 7);
+    Console.WriteLine($"è uscito il numero {dado}");
+    return dado;
 }
-else if (tiroUtente< tiroComputer)
+
+// La funzione stampa chi ha vinto confrontando i numeri che sono usciti
+void StampaVincitore(int tiroUtente, int tiroComputer)
 {
-    Console.WriteLine("HO VINTO IO");
+    if (tiroUtente > tiroComputer)
+    {
+        Console.WriteLine("HAI VINTO TU");
+    }
+    else if (tiroUtente < tiroComputer)
+    {
+        Console.WriteLine("HO VINTO IO");
+    }
+    else
+    {//chiedo all'utente se vuole giocare ancora
+        Console.WriteLine("Abbiamo pareggiato");
+    }
 }
-else
-{//chiedo all'utente se vuole giocare ancora
-    Console.WriteLine("Abbiamo pareggiato");
-   
-}
- Console.WriteLine("vuoi riprovare? s/n");
- rispostaUtente= Console.ReadLine();
-}  
-while ( rispostaUtente == "s");
+
+#endregion
 
 
 
