642d90998f3538d5f74860618fc076347a0299f4
annapintoanita <annapintoanita03@gmail.com>
2024-11-14 15:53:20 +0100

    tiro dei dadi

--- 04 - Assignment/_calcolatrice-con-funzioni/Program.cs
+++ 04 - Assignment/_calcolatrice-con-funzioni/Program.cs
@@ -1,29 +1,15 @@
 ﻿
-double numero1 = ChiediNumero();
-string operazione = ChiediOperazione();
-double numero2 = ChiediNumero();
-double risultato = 0;
-double StampaRisultato;
 
-risultato = StampaRisultato();
+double numero1 = ChiediNumero("Inserisci il primo numero");
+string operazione = ChiediOperazione();
+double numero2 = ChiediNumero("Inserisci il secondo numero");
+double risultato = 0;
+
 if (numero2 == 0 && operazione == "/")
 {
     Console.WriteLine("Mi dispiace, non puoi dividere per 0");
-
 }
-Console.WriteLine("Ciao, scegli un numero: ");
-double numero1 = 0;
-numero1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("ora scegli un' operazione: +, -, *, /");
-string operazione = Console.ReadLine();
-
-Console.WriteLine("Infine, scegli un altro numero: ");
-double numero2 = 0;
-numero2 = Convert.ToDouble(Console.ReadLine());
-
-
-risultato = Convert.ToDouble(Console.ReadLine());
 switch (operazione)
 {
     case "+":
@@ -43,19 +29,22 @@ switch (operazione)
         break;
 
 }
-Console.WriteLine($"il risultato è : {numero1} {operazione} {numero2} = {risultato}");
+StampaRisultato();
 
-double ChiediNumero();
+double ChiediNumero(string messaggio)
 {
-    Console.WriteLine("Ciao, scegli un numero: ");
+    Console.WriteLine(messaggio);
     return Convert.ToDouble(Console.ReadLine());
 }
 
-string ChiediOperazione();
+string ChiediOperazione()
 {
     Console.WriteLine("Ora scegli un'operazione: ");
     return Console.ReadLine();
 }
 
-void StampaRisultato();
+void StampaRisultato()
+{
+    Console.WriteLine($"il risultato è : {numero1} {operazione} {numero2} = {risultato}");
+}
 
--- 04 - Assignment/_calcolatrice-con-funzioni/README.md
+++ 04 - Assignment/_calcolatrice-con-funzioni/README.md
@@ -108,3 +108,57 @@ git add --all
 git commit -m "Implementata la versione 1 della calcolatrice con funzioni"
 git push -u origin main
 ```
+
+## Versione 1.2
+
+```csharp
+
+double numero1 = ChiediNumero("Inserisci il primo numero");
+string operazione = ChiediOperazione();
+double numero2 = ChiediNumero("Inserisci il secondo numero");
+double risultato = 0;
+
+if (numero2 == 0 && operazione == "/")
+{
+    Console.WriteLine("Mi dispiace, non puoi dividere per 0");
+}
+
+switch (operazione)
+{
+    case "+":
+        risultato = numero1 + numero2;
+        break;
+
+    case "-":
+        risultato = numero2 - numero1;
+        break;
+
+    case "*":
+        risultato = numero1 * numero2;
+        break;
+
+    case "/":
+        risultato = numero1 / numero2;
+        break;
+
+}
+StampaRisultato();
+
+double ChiediNumero(string messaggio)
+{
+    Console.WriteLine(messaggio);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+string ChiediOperazione()
+{
+    Console.WriteLine("Ora scegli un'operazione: ");
+    return Console.ReadLine();
+}
+
+void StampaRisultato()
+{
+    Console.WriteLine($"il risultato è : {numero1} {operazione} {numero2} = {risultato}");
+}
+
+```
