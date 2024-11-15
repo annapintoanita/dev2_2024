0b3a2bab3250fa635b4a0eae14c4e993b75e1ddf
annapintoanita <annapintoanita03@gmail.com>
2024-11-15 12:02:56 +0100

    versione4

--- 04 - Assignment/07_Gioco-di-dai/Program.cs
+++ 04 - Assignment/07_Gioco-di-dai/Program.cs
@@ -2,10 +2,17 @@
 string rispostaUtente;
 int punteggioUtente = 100;
 int punteggioComputer = 100;
+//dichiaro la variabile per il lancio dei dadi
+int lancioDadi;
+
+List<int> storicoPunteggioUtente = new List<int>();
+List<int> storicoPunteggioComputer = new List<int>();
+
 
 Random random = new Random();
 do
 {
+
     //genero numeri random 
 
     // presento il gioco all'utente
@@ -23,7 +30,10 @@ do
     // il computer tira
     Console.WriteLine("Tocca a me. Sto lanciando il dado.. ");
     int tiroComputer = LancioDado(random);
-    Console.WriteLine($" Per ora ho totalizzato {tiroComputer} punti");
+    Console.WriteLine($" Ho totalizzato {tiroComputer} punti");
+
+ 
+
 
     StampaVincitore(tiroUtente, tiroComputer, punteggioUtente, punteggioComputer);
 
@@ -31,6 +41,7 @@ do
     rispostaUtente = Console.ReadLine();
 }
 while (rispostaUtente == "s");
+MostraPunteggio(storicoPunteggioUtente,storicoPunteggioComputer);
 
 #region Funzioni
 
@@ -44,7 +55,7 @@ int LancioDado(Random random)
 }
 
 // La funzione stampa chi ha vinto confrontando i numeri che sono usciti
-void StampaVincitore(int tiroUtente, int tiroComputer, int  punteggioUtente, int punteggioComputer)
+void StampaVincitore(int tiroUtente, int tiroComputer, int punteggioUtente, int punteggioComputer)
 
 {
 
@@ -69,8 +80,25 @@ void StampaVincitore(int tiroUtente, int tiroComputer, int  punteggioUtente, int
     {//chiedo all'utente se vuole giocare ancora
         Console.WriteLine("Abbiamo pareggiato");
     }
-
+//richiamo la funzione AggiungiPunteggio
+ AggiungiPunteggio( punteggioUtente, punteggioComputer,storicoPunteggioUtente, storicoPunteggioComputer);
 }
+
+void MostraPunteggio(List<int> storicoPunteggioUtente, List<int> storicoPunteggioComputer )
+
+{
+    for (int i = 0; i < storicoPunteggioUtente.Count; i++)
+    {
+       Console.WriteLine($"{i} {storicoPunteggioComputer[i]}, {storicoPunteggioUtente[i]}");
+     
+    }
+}
+ 
+ void AggiungiPunteggio(int punteggioUtente, int punteggioComputer,List<int> storicoPunteggioUtente, List<int> storicoPunteggioComputer)
+ {
+    storicoPunteggioUtente.Add(punteggioUtente);
+    storicoPunteggioComputer.Add(punteggioComputer);
+ }
 
 #endregion
 
