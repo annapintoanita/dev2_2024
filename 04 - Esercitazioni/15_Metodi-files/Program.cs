30c5126c492fe81251b0836fa48b86ff1a1f8a69
annapintoanita <annapintoanita03@gmail.com>
2024-11-29 11:50:39 +0100

    implemento metodi files

--- 04 - Esercitazioni/15_Metodi-files/Program.cs
+++ 04 - Esercitazioni/15_Metodi-files/Program.cs
@@ -4,8 +4,33 @@
 string path = @"test.txt";
 File.Create(path).Close();
 
+//creare un file con il timestamp
+string path= $"test_{DateTime.Now.ToString("yyyyMMddHHmmss")}.txt";
+File.Create(path).Close();
+
 //Scrivere su un file
 File.WriteAllText(path, "Hello,World");
+
+//Scrivere una lista di stringhe su un file
+string[]lines ={"line1", "line2", "line3"};
+File.WriteAllLines(path, lines);
+
+//Aggiungere testo ad un file
+File.AppendAllText(path, "Hello, world");
+
+//aggiungere una lista di stringhe ad un file
+string[] lines = {"line1", "line2", "line3"};
+File.AppendAllLines(path, lines);
+
+//WriteAllText sovrascrive il contenuto del file, AppendAllText aggiunge il testo alla fine del file
+//WriteAllLines sovrascrive il contenuto del file, AppendAllLines aggiunge le righe alla fine del file
+//WruteAllText e AppendAllText sono equivalenti a scrivere con StreamWriter e usare il metodo Write o WriteLine rispettivamente
+
+//scrittura con StreamWriter
+using (StreamWriter sw = new StreamWriter(path))
+{
+    sw.Write("Hello,World");
+}
 
 //Leggere da un file
 string content = File.ReadAllText(path);
@@ -31,10 +56,58 @@ Directory.CreateDirectory(dir);
 Directory.Delete(dir);
 
 //Creare un file temporaneo
-string tempFile= path.GetTempFileName();
-Console.WriteLine(tempFile);
+string File= path.GetTempFileName();
+Console.WriteLine(File);
 
 //Creare un file temporaneo in una directory specifica
 //Path.Combine unisce i path in questo caso aggiunge "temp" alla directory temporanea
 string tempDir= path.Combine(path.GetTempPath(), "temp");
 Directory.CreateDirectory(tempDir);
+
+//verificare se un file esiste (restituisce un valore booleano)
+if (File.Exists(path))
+{
+    Console.WriteLine("File.exists");
+}
+
+//verificare se una directory esiste
+if(Directory.Exists(dir))
+{
+Console.WriteLine("Directory exists");
+}
+
+//ottenere informazioni su un file
+FileInfo info = new FileInfo(path);
+Console.WriteLine
+
+//ottenere informazione su una Directory
+DirectoryInfo dirInfo= new DirectoryInfo(dir);
+Console.WriteLine(dirInfo.CreationTime);
+
+//ottenere informazioni su tutti i file in una directory
+string[] files = DirectoryInfo.GetFiles(dir);
+foreach (string file in files)
+{
+    Console.WriteLine(file);
+}
+
+//ottenere informazioni su tutte le directory in una directory
+string[] dirs= DirectoryInfo.GetDirectories(dir);
+foreach (string d in dirs)
+{
+    Console.WriteLine(d);
+}
+
+//ottenere informazioni su tutti i file e le ditectory in una directory
+string[] all = Directory.GetFileSystemEntries(dir);
+foreach (string a in all)
+{
+    Console.WriteLine(a);
+}
+
+//ottenere informazioni su tutti i file e le directory in una directory con un filtro
+string[] textFiles= DirectoryInfo.GetFiles(dirs, "*.txt");
+foreach (string txtFile in textFiles)
+{
+    Console.WriteLine(txtFile);
+}
