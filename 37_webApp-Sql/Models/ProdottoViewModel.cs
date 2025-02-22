0b8e238bfaeaa98f49d6e8640aa1b6b62af0ee21
Davide Arecco <annapintoanita03@gmail.com>
2025-02-22 19:18:27 +0100

    modifiche nel readme logicaWebAPP

--- 37_webApp-Sql/Models/ProdottoViewModel.cs
+++ 37_webApp-Sql/Models/ProdottoViewModel.cs
@@ -3,13 +3,10 @@ public class ProdottoViewModel
 {
     public int Id { get; set; }
 
-
     public string Nome { get; set; }
 
- 
     public double Prezzo { get; set; }
 
-  
     public string CategoriaNome { get; set; }
   
     public string FornitoreNome { get; set; }
